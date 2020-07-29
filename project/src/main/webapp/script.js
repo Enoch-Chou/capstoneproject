469aeea7073ad5e2db6d9a8b34b64a4251fabfa7
jornichol <jordandavid253@gmail.com>
2020-07-29 22:38:17 +0000

    Javascript implementation of keyword extraction.

--- project/src/main/webapp/script.js
+++ project/src/main/webapp/script.js
@@ -1,28 +1,21 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+async function keywordExtraction(userInput) {
+    var result = "";
+    var count = 0;
+    var searchQuery = userInput.split(" ");
+    for (word of searchQuery){
+        if ((searchQuery.length-1) == count && word.indexOf('?') != -1) {
+                word = word.substring(0, word.length-1);
+            }
+        var url = new URL("https://api.datamuse.com/words?md=p&sp=" + word + "&max=1");
+        const output = await fetch(url).then(response => response.text());
+        if (output.includes("[\"n\"]") == true || output.includes("[\"adj\"]") == true){
+            result += word;
+            if (count != (searchQuery.length-1)){
+                result += " ";
+            }
+        }
+        count++;
+    }
+    return result;
 }
+
