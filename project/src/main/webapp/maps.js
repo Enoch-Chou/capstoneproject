b5746b9c1d8f23cf27aecb88d11514ceb010fe8d
jornichol <jordandavid253@gmail.com>
2020-08-06 22:02:27 +0000

    Display map based on ML search location answer.

--- project/src/main/webapp/maps.js
+++ project/src/main/webapp/maps.js
@@ -3,27 +3,34 @@ let service;
 let infowindow;
 
 function initMap() {
-    infowindow = new google.maps.InfoWindow();
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: google.maps.LatLng(-33.867, 151.195),
-        zoom: 15
-    });
+    if (editedKeywordExtraction(document.getElementById('query').value) == 'Place'){
+        document.getElementById('type').value = "Question Type: Place";
+        infowindow = new google.maps.InfoWindow();
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: google.maps.LatLng(-33.867, 151.195),
+            zoom: 15
+        });
 
-    var search = document.getElementById('query').value;
-    const request = {
-        query: search,
-        fields: ["name", "geometry"]
-    };
+        console.log(document.getElementById('displayAnswer').value);
+        var search = document.getElementById('displayAnswer').value;
+        console.log(search);
+        const request = {
+            query: search,
+            fields: ["name", "geometry"]
+        };
 
-    service = new google.maps.places.PlacesService(map);
-    service.findPlaceFromQuery(request, (results, status) => {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (let i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-        }
-        map.setCenter(results[0].geometry.location);
-        }
-    });
+        service = new google.maps.places.PlacesService(map);
+        service.findPlaceFromQuery(request, (results, status) => {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (let i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
+            map.setCenter(results[0].geometry.location);
+            }
+        });
+    } else if (editedKeywordExtraction(document.getElementById('query').value) == 'Time'){
+        document.getElementById('type').value = "Question Type: Time";
+    }
 }
 
 function createMarker(place) {
