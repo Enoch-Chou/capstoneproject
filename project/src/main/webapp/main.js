9745a3e7ef13ad5d17928647d280960ce5bad82d
Enoch Chou <enochs.chou@gmail.com>
2020-08-24 17:16:08 +0000

    Add a check for no questions entered

--- project/src/main/webapp/main.js
+++ project/src/main/webapp/main.js
@@ -1,6 +1,10 @@
-
 function main() {
-    const gmail = new gmailAPI(getQuery());
+    const query = getQuery();
+    if (query.length == 0) {
+        console.log("Please enter a question!");
+        return;
+    }
+    const gmail = new gmailAPI(query);
     listMessages(gmail).then(() => mlClass.parseEmailsWithModel(gmail)
     );
 }
