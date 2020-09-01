af484d2a3a141b103e87e4b777f8714fe0a2b360
jornichol <jordandavid253@gmail.com>
2020-09-01 20:06:46 +0000

    Add comments and rename function names.

--- project/src/main/webapp/keyword.js
+++ project/src/main/webapp/keyword.js
@@ -1,4 +1,5 @@
-function editedKeywordExtraction(userInput) {
+/* Classify type of question into location-based or time-based questions*/
+function classifyQuestionType(userInput) {
     var searchQuery = userInput.split(" ");
     if (searchQuery[0].toLowerCase() == 'where') {
         return 'Place';
@@ -9,19 +10,20 @@ function editedKeywordExtraction(userInput) {
     }
 }
 
-async function keywordExtraction(userInput) {
+/* A Javascript implementation of the keyword extraction algorithm that identifies keywords based on parts of speech.*/
+async function datamuseKeywordExtraction(userInput) {
     var result = "";
     var count = 0;
     var searchQuery = userInput.split(" ");
     for (word of searchQuery){
-        if ((searchQuery.length-1) == count && word.indexOf('?') != -1) {
+        if ((searchQuery.length - 1) == count && word.indexOf('?') != -1) {
                 word = word.substring(0, word.length-1);
             }
         var url = new URL("https://api.datamuse.com/words?md=p&sp=" + word + "&max=1");
         const output = await fetch(url).then(response => response.text());
         if (output.includes("[\"n\"]") == true || output.includes("[\"adj\"]") == true){
             result += word;
-            if (count != (searchQuery.length-1)){
+            if (count != (searchQuery.length - 1)){
                 result += " ";
             }
         }
