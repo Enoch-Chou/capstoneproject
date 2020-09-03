a01b3e2bc9e09083779535d267fc16514a0ed6b4
jornichol <jordandavid253@gmail.com>
2020-09-03 23:17:06 +0000

    fix indentation

--- project/src/main/webapp/maps.js
+++ project/src/main/webapp/maps.js
@@ -2,8 +2,8 @@
 function initMap() {
     if (classifyQuestionType(document.getElementById('query').value) === 'Place'){
         document.getElementById("map").style.display = "block";
-        let infowindow = new google.maps.InfoWindow();
-        let map = new google.maps.Map(document.getElementById("map"), {
+        const infowindow = new google.maps.InfoWindow();
+        const map = new google.maps.Map(document.getElementById("map"), {
             center: google.maps.LatLng(-33.867, 151.195),
             zoom: 15
         });
@@ -14,14 +14,14 @@ function initMap() {
             fields: ["name", "geometry"]
         };
 
-        let service = new google.maps.places.PlacesService(map);
+        const service = new google.maps.places.PlacesService(map);
         service.findPlaceFromQuery(request, (results, status) => {
             if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (let i = 0; i < results.length; i++) {
-                createMarker(results[i], map, infowindow);
-            }
-            map.setCenter(results[0].geometry.location);
-            }
+                for (let i = 0; i < results.length; i++) {
+                    createMarker(results[i], map, infowindow);
+                }
+                map.setCenter(results[0].geometry.location);
+                }
         });
     } else {
         document.getElementById("map").style.display = "none";
